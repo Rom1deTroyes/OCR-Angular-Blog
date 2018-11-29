8a3b5c6bc8707729e1c485ce48b43c57213b15f8
Romain HELLER <r.heller@epack-hygiene.fr>
2018-11-29 10:17:58 +0100

    add component PostList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
