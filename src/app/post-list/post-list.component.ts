0709b0d1af6b0262572eda0fd52df659ffe28b6b
Romain HELLER <r.heller@epack-hygiene.fr>
2018-11-30 09:14:37 +0100

    Post-List : get posts array on Input and show a PostListItem for each post

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
+
+
+  @Input() posts: Array<Object> ;
+
 
   constructor() { }
 
